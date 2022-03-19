ab9ca5eea4b2a686242550ac27fe10d41300cb22
Piyachat <6314352@g.cmru.ac.th>
2022-03-19 15:22:54 +0700

    ok

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 d-flex justify-content-center"> 
+        <div class="text-center" style="background-color: rgba(179, 59, 59, 0.795);">
+        <img src="https://ichef.bbci.co.uk/news/640/cpsprodpb/1124F/production/_119932207_indifferentcatgettyimages.png"
+          width="250"> <br><br>
+         <h4>I N F O</h4>
+        <hr>
+       <p>
+        <h6> Name :</h6>
+        Piyachat Seeda
+      </p>
+      <p>
+        <h6> Phone :</h6>
+        0905967779
+      </p>
+      <p>
+        <h6> E-mail :</h6>
+        63143252@g.cmru.ac.th
+      </p><br>
+
+        <h4> S O C I A L  :</h4>
+      <hr>
+      <p>
+      <h6>Facebook :</h6>
+      <a href="https://www.facebook.com/jenny.nyny0/" style="text-decoration: none;"> Piyachat Seeda </a>
+    </p>
+      <p>
+        <h6> Instagram:</h6>
+        <a href="https://www.instagram.com/piyachat.jn/" style="text-decoration: none;"> piyachat.jn </a>
+      </p>
+      </div> 
+
+
+
+    </div>
+  </div>
+</body>
+
+</html>
